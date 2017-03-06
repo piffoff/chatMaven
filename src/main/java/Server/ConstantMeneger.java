6a010834bd89df9ec4ccf18e88d0481bb55b2acb
piffoff <jiamepok2008@yandex.ru>
2017-03-06 11:00:09 +0300

    add posgresql

--- src/main/java/Server/ConstantMeneger.java
+++ src/main/java/Server/ConstantMeneger.java
@@ -8,12 +8,13 @@ public class ConstantMeneger {
     public static final String dbUrl = "jdbc:postgresql://localhost:5432/postgres";
     public static final String user = "postgres";
     public static final String password = "postgres";
+
+//    public static final String dbUrl = "jdbc:postgresql://server1:5432/postgres";
+//    public static final String user = "postgres";
+//    public static final String password = "postgres";
+
     /**
      * 1-st Parameter Login, 2-nd Password
      */
     public static final String SQLSelect = "SELECT Nickname FROM main WHERE Login = ? AND Password = ?;";
-//    private static final String dbUrl = "jdbc:postgresql://server1:5432/postgres";
-//    private static final String user = "postgres";
-//    private static final String password = "******";
-
 }
